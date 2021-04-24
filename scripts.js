90ce4b8ac5d3125b984ffb64744f7a4c66c759d1
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2021-04-24 10:53:24 -0300

    Primeiros passos JS

---
+++ scripts.js
@@ -0,0 +1,22 @@
+console.log('AULA DE JAVASCRIPT')
+console.log("Primeiros passos ao mundo JS")
+
+//DECLARAÇÃO DE VARIÁVEIS
+//Boas práticas colocar nomeVariavel
+
+//LET PERTMITE A ALTERAÇÃO DOS VALORES 
+let valorIngresso = 20;
+valorIngresso = 50;
+console.log("O valor do Ingresso é além de cara: R$" + valorIngresso)
+
+//CONST NÃO PERMITE ALTERAÇÃO
+const valorPastel = 30;
+console.log("O Valor do Pastel é: R$" + valorPastel)
+
+
+let nome = 'Gustavo'; //STRING LITERAL
+let idade = 26; //NUMBER LITERAL
+let voceEstaAprovado = true; //BOOLEAN (REGRA DE APLICAÇÃO)
+let sobrenome = undefined; //UNDEFINED
+let corSelecionar = null; //QUANDO QUER REDEFINIR UM VALOR
+
